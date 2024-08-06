8ed7caf342dccb54a934cd0b97074702a03bd444
Miquéias Silva <d.miqueias@academico.ifg.edu.br>
2024-08-06 18:52:50 -0300

    Commit inicial

---
+++ src/test/java/com/example/seminariojenkins/SeminarioJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seminariojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarioJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
