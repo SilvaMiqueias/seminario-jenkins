e1b15592fb1791d153727b075c1585bc5a26ae90
Miquéias Silva <d.miqueias@academico.ifg.edu.br>
2024-08-06 19:14:25 -0300

    Commit inicial

---
+++ src/main/java/com/example/seminariojenkins/SeminarioJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.seminariojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarioJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeminarioJenkinsApplication.class, args);
+    }
+
+}
